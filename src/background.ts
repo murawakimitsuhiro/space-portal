b3e4e3f9c15b01b4c242269dee5f0902ea695a18
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2022-06-14 20:29:31 +0900

    [add] init template by chrome-extension-cli

---
+++ src/background.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+// With background scripts you can communicate with popup
+// and contentScript files.
+// For more information on background script,
+// See https://developer.chrome.com/extensions/background_pages
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'GREETINGS') {
+    const message: string = `Hi ${
+      sender.tab ? 'Con' : 'Pop'
+    }, my name is Bac. I am from Background. It's great to hear from you.`;
+
+    // Log message coming from the `request` parameter
+    console.log(request.payload.message);
+    // Send a response message
+    sendResponse({
+      message,
+    });
+  }
+});
+
+{
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: 'uploadToSpace',
+      title: 'リンク先を空間にアップロードする'
+    });
+  });
+
+  // メニューをクリック時に実行
+  chrome.contextMenus.onClicked.addListener(item => {
+    console.log(item);
+    console.log(item.menuItemId);
+  });
+}
